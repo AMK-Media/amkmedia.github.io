e7ee0cce12a3444c9c2bf41ba7dab1e36ef45cd1
Aaron Kendall <149661442+AMK-Media@users.noreply.github.com>
2023-11-18 09:25:54 +0000

    1

---
+++ index.php
@@ -0,0 +1,4 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6073009479701905"
+     crossorigin="anonymous"></script>
+     google.com, pub-6073009479701905, DIRECT, f08c47fec0942fa0
+     <meta name="google-adsense-account" content="ca-pub-6073009479701905">
